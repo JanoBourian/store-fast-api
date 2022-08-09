0d826f7c182d8b8ed15d3af0f56b52d36f708be8
JanoBourian <supermath33@icloud.com>
2022-08-08 19:37:25 -0500

    Add: some tests

---
+++ test/test_root.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from app.main import app
+client = TestClient(app)
+
+## get("/")
+
+def test_get_all_registries():
+    response = client.get("/")
+    assert response.status_code == 200
+    
+## get("/id")
+
+def test_get_registry_no_exist():
+    response = client.get("/1")
+    assert response.status_code == 404
